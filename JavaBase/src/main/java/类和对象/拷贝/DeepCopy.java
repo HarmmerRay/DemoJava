31554fe2286a9a84fcc026c75ad4a8672a9b7a7d
HarmmerRay <zhao131804@gmail.com>
2024-03-04 09:14:42 +0800

    #1 [基础][基础知识点][2024.3.4] 浅拷贝、深拷贝

--- JavaBase/src/main/java/类和对象/拷贝/DeepCopy.java
+++ JavaBase/src/main/java/类和对象/拷贝/DeepCopy.java
@@ -1,9 +1,44 @@
 package 类和对象.拷贝;
 
+import org.apache.commons.lang3.SerializationUtils;
+
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  * @description:
  * @author: HammerRay
  * @date: 2024/3/4 上午8:01
  */
 public class DeepCopy {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "John";
+        person1.address = new Address();
+        person1.address.city = "New York";
+
+        Person person2 = person1.deepCopy();
+        // 引用地址不相同 false
+        System.out.println(person2 == person1);
+        // 引用地址指向的对象也不相同 false
+        System.out.println(person2.address == person1.address);
+    }
+}
+
+class Person implements Serializable{
+    String name;
+    Address address;
+    /**
+     * description: 用序列化和反序列化实现深拷贝 ，Person类实现Serializable 接口
+     * @paramType []
+     * @returnType: 类和对象.拷贝.Person
+     * @author: GodHammer
+     * @date: 2024-03-04 上午8:41
+     */
+    public Person deepCopy(){
+        return SerializationUtils.clone(this);
+    }
+}
+class Address implements Serializable{
+    String city;
 }
--- JavaBase/src/main/java/类和对象/拷贝/ShallowCopy.java
+++ JavaBase/src/main/java/类和对象/拷贝/ShallowCopy.java
@@ -1,15 +1,36 @@
-package 类和对象.拷贝;
-
-/**
- * @description:
- * @author: HammerRay
- * @date: 2024/3/4 上午8:01
- */
-public class ShallowCopy implements Cloneable {
-    String name;
-    String address;
-    @Override
-    protected Object clone() {
-        return super.clone();
-    }
-}
+//package 类和对象.拷贝;
+//
+///**
+// * @description:
+// * @author: HammerRay
+// * @date: 2024/3/4 上午8:01
+// */
+//public class ShallowCopy {
+//    public static void main(String[] args) throws CloneNotSupportedException {
+//        Person person1 = new Person();
+//        person1.name = "John";
+//        person1.address = new Address();
+//        person1.address.city = "New York";
+//
+//        Person person2 = (Person) person1.clone();
+//
+//        // false 拷贝的是引用 不是对象  所以 引用不同
+//        System.out.println(person1 == person2);
+//        // true    引用指向的对象相同
+//        System.out.println(person1.address == person2.address);
+//    }
+//}
+//
+//class Person implements Cloneable {
+//    String name;
+//    Address address;
+//
+//    @Override
+//    protected Object clone() throws CloneNotSupportedException {
+//        return super.clone();
+//    }
+//}
+//
+//class Address {
+//    String city;
+//}
