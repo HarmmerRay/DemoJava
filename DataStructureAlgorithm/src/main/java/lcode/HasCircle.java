56754fd0649ca81d23cbab1e135664ec04b54dd8
HarmmerRay <zhao131804@gmail.com>
2024-04-15 22:37:16 +0800

    #1[基础][算法][2024.4.15] HasCircle

---
+++ DataStructureAlgorithm/src/main/java/lcode/HasCircle.java
@@ -0,0 +1,22 @@
+package lcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @description:
+ * @author: HammerRay
+ * @date: 2024/4/15 下午10:36
+ */
+public class HasCircle {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> seen = new HashSet<ListNode>();
+        while (head != null) {
+            if (!seen.add(head)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+}
