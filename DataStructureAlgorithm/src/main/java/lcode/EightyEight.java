b2bd96d779ec74c673729f2d9e4c808da1f753e3
HarmmerRay <zhao131804@gmail.com>
2024-04-16 23:06:21 +0800

    #1[基础][算法][2024.4.16] MergeSortedArray、IsValid

--- DataStructureAlgorithm/src/main/java/lcode/EightyEight.java
+++ DataStructureAlgorithm/src/main/java/lcode/EightyEight.java
@@ -7,13 +7,52 @@ package lcode;
  */
 public class EightyEight {
     public static void main(String[] args) {
-        int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int m = 3;
-        int[] nums2 = {2, 5, 6};
-        int n = 3;
-        merge(nums1, m, nums2, n);
+//        int[] nums1 = {1, 2, 3, 0, 0, 0};
+//        int m = 3;
+//        int[] nums2 = {2, 5, 6};
+//        int n = 3;
+//        merge(nums1, m, nums2, n);
+        int[] num1 = {0};
+        int m = 0;
+        int[] num2 = {1};
+        int n = 1;
+        mergeB(num1,m,num2,n);
     }
-
+    public static void mergeB(int[] nums1, int m, int[] nums2, int n) {
+        if(m == 0 && n != 0){
+            for(int i = 0;i < n;i++){
+                nums1[i] = nums2[i];
+            }
+            return;
+        }
+        if(m == 0 && n == 0){
+            return;
+        }
+        if(m != 0 && n == 0){
+            return;
+        }
+        for(int i = n - 1,j = m - 1,k = m+n-1 ; k >= 0;){
+            if(i == -1 ){
+                return;
+            }
+            if(j == -1){
+                nums1[k] = nums2[i];
+                k--;
+                i--;
+                continue;
+            }
+            if(nums2[i] >= nums1[j]){
+                nums1[k] = nums2[i];
+                k--;
+                i--;
+            }else{
+                nums1[k] = nums1[j];
+                nums1[j] = -1;
+                k--;
+                j--;
+            }
+        }
+    }
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
 //        // 正向双指针 时间复杂度：O(m+n) 空复:O(m+n)
 //        int[] tmp = new int[m + n];
--- DataStructureAlgorithm/src/main/java/lcode/IsValid.java
+++ DataStructureAlgorithm/src/main/java/lcode/IsValid.java
@@ -1,6 +1,6 @@
 package lcode;
 
-import java.util.Stack;
+import java.util.*;
 
 /**
  * @description:
@@ -8,16 +8,39 @@ import java.util.Stack;
  * @date: 2024/4/14 下午3:56
  */
 public class IsValid {
-    public static boolean isValid(String s){
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            stack.add(s.charAt(i));
-        }
+    private static Map<Character, Character> map = new HashMap() {{
+        put('(', ')');
+        put('[', ']');
+        put('{', '}');
+        put('?', '?');
+    }};
 
-        return true;
+    private static boolean isValid(String s) {
+        if(!s.isEmpty() && !map.containsKey(s.charAt(0))) {
+            return false;
+        }
+        LinkedList<Character> stack = new LinkedList<Character>() {{
+            add('?');
+        }};
+        for (Character c: s.toCharArray()) {
+            if (map.containsKey(c)){
+                stack.addLast(c);
+            }else if(!map.get(stack.removeLast()).equals(c)) {
+                return false;
+            }
+        }
+        
+        return stack.size() == 1;
     }
 
     public static void main(String[] args) {
-
+//        String s = "()";
+//        System.out.println(isValid(s));
+        int []nums1 = {1,2,3,4};
+        int []nums2 = {2,2,2,2};
+        nums1 = nums2;
+        for (Integer i:nums1) {
+            System.out.println(i);
+        }
     }
 }
