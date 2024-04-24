0ef46f3747d65be87405fae82f335ec6703d3eda
HarmmerRay <zhao131804@gmail.com>
2024-04-24 23:37:46 +0800

    #1 [基础][算法][2024.4.24] ZConver

--- DataStructureAlgorithm/src/main/java/lcode/ZConvert.java
+++ DataStructureAlgorithm/src/main/java/lcode/ZConvert.java
@@ -1,42 +1,26 @@
 package lcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * @projectName: DemoJava
- * @package: lcode
- * @className: Zconvert
+ * @description: leetcode 6. Z字形变换
  * @author: HammerRay
- * @description: TODO
- * @date: 4/17/2024 11:26 PM
- * @version: 1.0
+ * @date: 2024/4/24 下午11:36
  */
-
 public class ZConvert {
-        public String convert(String s, int numRows) {
-            int n = s.length(), r = numRows;
-            if (r == 1 || r >= n) {
-                return s;
-            }
-            int t = r * 2 - 2;
-            int c = (n + t - 1) / t * (r - 1);
-            char[][] mat = new char[r][c];
-            for (int i = 0, x = 0, y = 0; i < n; ++i) {
-                mat[x][y] = s.charAt(i);
-                if (i % t < r - 1) {
-                    ++x; // 向下移动
-                } else {
-                    --x;
-                    ++y; // 向右上移动
-                }
-            }
-            StringBuffer ans = new StringBuffer();
-            for (char[] row : mat) {
-                for (char ch : row) {
-                    if (ch != 0) {
-                        ans.append(ch);
-                    }
-                }
-            }
-            return ans.toString();
+    public String convert(String s, int numRows) {
+        if(numRows < 2) return s;
+        List<StringBuilder> rows = new ArrayList<StringBuilder>();
+        for(int i = 0; i < numRows; i++) rows.add(new StringBuilder());
+        int i = 0, flag = -1;
+        for(char c : s.toCharArray()) {
+            rows.get(i).append(c);
+            if(i == 0 || i == numRows -1) flag = - flag;
+            i += flag;
         }
-
+        StringBuilder res = new StringBuilder();
+        for(StringBuilder row : rows) res.append(row);
+        return res.toString();
+    }
 }
