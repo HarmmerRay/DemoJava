ab9ed66888da58354b236b1ad5247199552f19fb
HarmmerRay <zhao131804@gmail.com>
2024-05-01 23:52:49 +0800

    #1 [基础][算法][2024.5.1] IsPalindrome

---
+++ DataStructureAlgorithm/src/main/java/lcode/IsPalindrome.java
@@ -0,0 +1,21 @@
+package lcode;
+
+/**
+ * @description:
+ * @author: HammerRay
+ * @date: 2024/5/1 下午11:52
+ */
+public class IsPalindrome {
+    public boolean isPalindrome(String s) {
+        StringBuffer sgood = new StringBuffer();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                sgood.append(Character.toLowerCase(ch));
+            }
+        }
+        StringBuffer sgood_rev = new StringBuffer(sgood).reverse();
+        return sgood.toString().equals(sgood_rev.toString());
+    }
+}
